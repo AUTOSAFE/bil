4815be63a6725c7fcbf589dac590dea906311974
AUTOSAFE <107992265+AUTOSAFE@users.noreply.github.com>
2022-08-06 21:24:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,105 @@
-const _0x5eb2cc=_0x1f6a;function _0x1a99(){const _0x28c178=['chalk','2768752rERwGw','error','stringify','parse','5448807ryxgYQ','blueBright','488821eOXQgM','Kamu\x20Perlu\x20Memberikan\x20keys\x20Auth\x20Sebagai\x20\x20Object','User','3383600FLtlHK','then','\x20|\x20','getHours','yellowBright','interval','async','11758sAjcJT','bold','Mozilla/5.0\x20(Windows\x20NT\x2010.0;\x20Win64;\x20x64))','2522932wctVal','figlet','prompt','138708LUOvRg','Interval\x20Delay\x20=','input','redBright','BANNED','catch','inquirer','145OLxzre','list','round','message','gzip','${color(\x22>>\x22,\x20\x22whiteBright\x22)}','3PRmLgM','Enter\x20Keys\x20Auth\x20\x27Round\x27\x20='];_0x1a99=function(){return _0x28c178;};return _0x1a99();}function _0x1f6a(_0x5cbd6f,_0x398564){const _0x1a997d=_0x1a99();return _0x1f6a=function(_0x1f6a27,_0x3878f5){_0x1f6a27=_0x1f6a27-0xcb;let _0xf45420=_0x1a997d[_0x1f6a27];return _0xf45420;},_0x1f6a(_0x5cbd6f,_0x398564);}(function(_0x4dfc84,_0x418d20){const _0x4bca92=_0x1f6a,_0x23431e=_0x4dfc84();while(!![]){try{const _0x1f4658=parseInt(_0x4bca92(0xdc))/0x1+parseInt(_0x4bca92(0xe6))/0x2*(parseInt(_0x4bca92(0xd3))/0x3)+parseInt(_0x4bca92(0xe9))/0x4+-parseInt(_0x4bca92(0xcd))/0x5*(-parseInt(_0x4bca92(0xec))/0x6)+-parseInt(_0x4bca92(0xd6))/0x7+-parseInt(_0x4bca92(0xdf))/0x8+-parseInt(_0x4bca92(0xda))/0x9;if(_0x1f4658===_0x418d20)break;else _0x23431e['push'](_0x23431e['shift']());}catch(_0x51dd97){_0x23431e['push'](_0x23431e['shift']());}}}(_0x1a99,0x5acea));const chalk=require(_0x5eb2cc(0xd5)),figlet=require(_0x5eb2cc(0xea)),inquirer=require(_0x5eb2cc(0xcc)),questions=[{'type':_0x5eb2cc(0xee),'name':'authorization','message':color('key\x20Auth\x20='),'prefix':''+color('-','greenBright'),'suffix':'','validate':function(_0x5de032){const _0x4e27b0=_0x5eb2cc,_0x39bb24=this[_0x4e27b0(0xe5)]();if(!_0x5de032)return _0x39bb24(_0x4e27b0(0xd2),_0x4e27b0(0xdd)),![];let _0x4b7de0;try{_0x4b7de0=JSON[_0x4e27b0(0xd9)](_0x5de032);}catch(_0x294639){_0x4b7de0=_0x294639[_0x4e27b0(0xd0)];}if(typeof _0x4b7de0!='object')return _0x39bb24('Kamu\x20Perlu\x20Memberikan\x20keys\x20Auth\x20Sebagai\x20Object'),![];return _0x39bb24(null,!![]);}},{'type':_0x5eb2cc(0xce),'name':_0x5eb2cc(0xcf),'message':color(_0x5eb2cc(0xd4)),'prefix':''+color('-',_0x5eb2cc(0xe3)),'suffix':'','choices':['Stage\x201','Stage\x202','Stage\x203'],'filter':_0x347c1d=>{return{'Stage\x201':0x1,'Stage\x202':0x2,'Stage\x203':0x3}[_0x347c1d];}},{'type':_0x5eb2cc(0xee),'name':_0x5eb2cc(0xe4),'message':color(_0x5eb2cc(0xed)),'prefix':''+color('-','redBright'),'suffix':'','validate':function(_0x18477b){const _0x4a296f=_0x5eb2cc,_0xa0f1d=this[_0x4a296f(0xe5)]();if(_0x18477b&&isNaN(_0x18477b))return _0xa0f1d('${color(\x22>>\x22,\x20\x22greenBright\x22)}\x20Kamu\x20Harus\x20Memasukkan\x20Delay'),![];return _0xa0f1d(null,!![]);}}];inquirer[_0x5eb2cc(0xeb)](questions)[_0x5eb2cc(0xe0)](async({authorization:_0x50223d,round:_0xd3762b,interval:_0x50a260})=>{const _0x229e50=_0x5eb2cc,_0x164b38=JSON[_0x229e50(0xd9)](_0x50223d);iStumble(_0x50a260,_0xd3762b,_0x164b38);});function iStumble(_0xd81b36,_0x540967,_0x303bc0){setInterval(async function _0x355ec1(){const _0x58c67c=_0x1f6a;try{const {data:_0x49eeee}=await stageRequest(_0x303bc0,_0x540967);if(typeof _0x49eeee=='string'&&_0x49eeee['includes'](_0x58c67c(0xf0)))console[_0x58c67c(0xd7)](color('BANNED',_0x58c67c(0xef)));else{if(typeof _0x49eeee=='object'){const _0x370e5c=new Date();let {Id:_0x3588b6,Username:_0x2feb72,Country:_0x577fdf,Region:_0x4c9e8e,Crowns:_0x4a21f3,SkillRating:_0x25c116}=_0x49eeee[_0x58c67c(0xde)];const _0x445432='['+color(_0x370e5c[_0x58c67c(0xe2)]())+':'+_0x370e5c['getMinutes']()+']\x20'+[color(_0x3588b6,_0x58c67c(0xdb)),color(_0x2feb72,'magenta'),color(_0x577fdf,_0x58c67c(0xdb)),color(_0x4c9e8e,_0x58c67c(0xdb)),color(_0x4a21f3,_0x58c67c(0xef)),color(_0x25c116,_0x58c67c(0xe3))]['join'](_0x58c67c(0xe1));console['log'](_0x445432);}}}catch(_0x528f59){}},Number(_0xd81b36));}function color(_0x57f8a5,_0x34091f){const _0x58e973=_0x5eb2cc;return _0x34091f?chalk[_0x34091f][_0x58e973(0xe7)](_0x57f8a5):chalk['Blue']['bold'](_0x57f8a5);}function stageRequest(_0x27d452,_0x2b5e9b){return new Promise((_0x57f163,_0x36e463)=>{const _0x54b0a2=_0x1f6a;request({'url':'http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/'+_0x2b5e9b,'headers':{'Authorization':JSON[_0x54b0a2(0xd8)](_0x27d452),'use_response_compression':!![],'Accept-Encoding':_0x54b0a2(0xd1),'User-Agent':_0x54b0a2(0xe8)}})[_0x54b0a2(0xe0)](_0x34d30d=>{_0x57f163(_0x34d30d);})[_0x54b0a2(0xcb)](_0x36e463);});}
+const chalk = require("chalk");
+const figlet = require("figlet");
+const inquirer = require("inquirer");
+
+const questions = [
+    {
+        type: "input",
+        name: "authorization",
+        message: color("key Auth ="),
+        prefix: `${color("-", "greenBright")}`,
+        suffix: "",
+        validate: function (input) {
+            const done = this.async();
+            if (!input) {
+                done('${color(">>", "whiteBright")}', 'Kamu Perlu Memberikan keys Auth Sebagai  Object');
+                return false;
+            }
+            let authParse;
+            try {
+                authParse = JSON.parse(input);
+            } catch (error) {
+                authParse = error.message;
+            }
+            if (typeof authParse != 'object') {
+                done('Kamu Perlu Memberikan keys Auth Sebagai Object');
+                return false;
+            }
+            return done(null, true);
+        },
+    },
+    {
+        type: "list",
+        name: "round",
+        message: color("Enter Keys Auth 'Round' ="),
+        prefix: `${color("-", "yellowBright")}`,
+        suffix: "",
+        choices: ["Stage 1", "Stage 2", "Stage 3"],
+        filter: (value) => {
+            return {
+                "Stage 1": 1,
+                "Stage 2": 2,
+                "Stage 3": 3,
+            }[value];
+        },
+    },
+    {
+        type: "input",
+        name: "interval",
+        message: color("Interval Delay ="),
+        prefix:`${color("-", "redBright")}`,
+        suffix: "",
+        validate: function (input) {
+            const done = this.async();
+            if (input && isNaN(input)) {
+                done('${color(">>", "greenBright")} Kamu Harus Memasukkan Delay');
+                return false;
+            }
+            return done(null, true);
+        },
+    }
+];
+
+inquirer.prompt(questions)
+    .then(async ({ authorization, round, interval }) => {
+        const authParse = JSON.parse(authorization);
+        iStumble(interval, round, authParse);
+    });
+
+function iStumble(interval, round, authorization) {
+    setInterval(async function iStumble() {
+        try {
+            const { data } = await stageRequest(authorization, round);
+            if (typeof data == "string" && data.includes("BANNED")) {
+                console.error(color("BANNED", "whiteBright"));
+            } else if (typeof data == "object") {
+                const date = new Date();
+                let { Id, Username, Country, Region, Crowns, SkillRating } = data.User;
+                const print = `[${color(date.getHours())}:${date.getMinutes()}] ` + [color(Id, "greenBright"), color(Username, "magenta"), color(Country, "yellowBright"), color(Region, "redBright"), color(Crowns, "whiteBright"), color(SkillRating, "yellowBright")].join(" | ");
+                console.log(print);
+            }
+        } catch (error) {}
+    }, Number(interval));
+}
+
+function color(text, color) {
+    return color ? chalk[color].bold(text) : chalk.Blue.bold(text);
+}
+
+function stageRequest(authorization, round) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: `http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/${round}`,
+            headers: {
+                Authorization: JSON.stringify(authorization),
+                use_response_compression: true,
+                "Accept-Encoding": "gzip",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64))",
+            }
+        })
+            .then((response) => {
+                resolve(response);
+            })
+            .catch(reject);
+    });
+}
